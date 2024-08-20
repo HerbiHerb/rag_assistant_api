fdcae15c8e7e4b0354cc68b07727f211e0891ea3
Dennis Herbrik <dennisherbrik1988@gmail.com>
2024-08-20 08:37:06 +0200

    Changed gmail tool

--- src/rag_assistant_api/test_gmail.py
+++ src/rag_assistant_api/test_gmail.py
@@ -2,10 +2,10 @@ from .agents.langchain.langchain_tools.tools import GetNewEmails, GoogleSearch
 
 
 if __name__ == "__main__":
-    # email_func = GetNewEmails()
+    email_func = GetNewEmails()
 
-    # email_func._run()
+    email_func._run()
 
     # test = 0
-    google_search = GoogleSearch()
-    google_search._run(search_term="Was hat Elon Musk studiert?")
+    # google_search = GoogleSearch()
+    # google_search._run(search_term="Was hat Elon Musk studiert?")
