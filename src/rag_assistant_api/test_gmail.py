7d41cd5d7b6aae1e62110741ea46b2a2b0020f92
Dennis Herbrik <dennisherbrik1988@gmail.com>
2024-08-24 21:35:12 +0200

    Little changes

--- src/rag_assistant_api/test_gmail.py
+++ src/rag_assistant_api/test_gmail.py
@@ -15,11 +15,6 @@ if __name__ == "__main__":
 
     # send_mail._run(subject="Erinnerung", message="Rufe das Krankenhaus an")
 
-    # test = 0
-    # google_search = GoogleSearch()
-    # google_search._run(search_term="Was hat Elon Musk studiert?")
-
-    youtube_search = YouTubeSearch(
-        youtube_api_key="AIzaSyCFttMvdBy2XCMPnoLavTEuCG2ef1j7TcA"
-    )
-    youtube_search._run(search_term="Lex Fridman and Max Tegmark")
+    test = 0
+    google_search = GoogleSearch()
+    google_search._run(search_term="Was hat Elon Musk studiert?")
